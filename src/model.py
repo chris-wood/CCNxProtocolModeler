0f9e27eeb4bd00a4138b075a56fbb4d5dd5842e0
Christopher Wood <cwood@capri-wlan.parc.xerox.com>
2013-08-07 11:06:45 -0700

    Updates to the model code...

--- src/model.py
+++ src/model.py
@@ -17,10 +17,9 @@ K = 0
 c = 0
 x = 0
 alpha = 0
+delta = 0
 sigma = 0
 sigma_k = []
-delta = 0
-delta_k = []
 lmbda = 0
 lmbda_k = []
 
@@ -61,15 +60,31 @@ def calc_p_filter(k, i, filtering = True):
 
 		Status: Correct.
 	'''
-	global lmbda, alpha, sigma, sigma_k, delta, delta_k, M, K, c, x
+	global lmbda, alpha, sigma, sigma_k, delta, M, K, c, x
 
 	if filtering: # for k \in {1,..,N}
 		bki = calc_bk(k, i, filtering)
-		bki_sigma = pow(bki, sigma_k[k - 1])
-		num = bki * ( 1.0 - bki_sigma )
-		denom = (1.0 - bki) * sigma_k[k - 1]
+		# bki_sigma = pow(bki, sigma_k[k - 1])
+
+		# technical report version
+		# num = bki * ( 1.0 - bki_sigma )
+
+		# paper version
+		num = 1.0 - bki
+
+		# technical report version
+		# denom = (1.0 - bki) * sigma_k[k - 1]
+
+		# paper version
+		denom = 1.0 - ((1.0 - (1.0 / sigma)) * bki)
+
 		frac = num / denom
-		return (1.0 - frac)
+
+		# tecnical report
+		# return (1.0 - frac)
+
+		# paper
+		return frac
 	else:
 		raise Exception("Non-filtering model not implemented.")
 
@@ -78,15 +93,15 @@ def calc_bk(k, i, filtering = True):
 
 		Status: Match.
 	'''
-	global lmbda, alpha, sigma, sigma_k, delta, delta_k, M, K, c, x
+	global lmbda, alpha, sigma, sigma_k, delta, M, K, c, x
 
 	if filtering:
 		m = float(M / K)
 		if (i == 1):
-			return exp( (delta_k[k - 1] * -1) * sigma_k[k - 1] / m )
-		else:
+			return exp( (calc_delta(k, i, filtering) * -1) * lmbda_k[k - 1] / m )
+		elif i > 1:
 			mu_k = calc_mu(k, i - 1, filtering)
-			return exp( (delta_k[k - 1] * -1) * 2.0 * mu_k / m)
+			return exp( 2.0 * (calc_delta(k, i, filtering) * -1) * mu_k / m)
 	else:
 		raise Exception("Non-filtering model not implemented.")
 
@@ -100,8 +115,10 @@ def calc_mu(k, i, filtering = True):
 	if filtering:
 		if (i == 1):
 			return (lmbda_k[k - 1] * calc_p1(k) * (1.0 - calc_p_filter(k, 1, filtering)))
-		else:
+		elif (i > 1):
 			return (2.0 * calc_mu(k, i - 1, filtering) * calc_p(k, i, filtering) * (1.0 - calc_p_filter(k, i, filtering)))
+		else:
+			raise Exception("Invalid router index: " + str(i))
 	else:
 		raise Exception("Non-filtering model not implemented.")
 
@@ -128,7 +145,9 @@ def calc_p(k, i, filtering = True): # assume filtering (interest aggregation) is
 		exponent = float(1.0) 
 		for l in range(1, i): #[1, i - 1]
 			p_k_l = calc_p(k, l, filtering)
-			p_filter_k_l = calc_p_filter(k, l)
+
+			print(str(i) + " - calculating filter for l = " + str(l))
+			p_filter_k_l = calc_p_filter(k, l, filtering)
 
 			# More sanity checks
 			if (p_k_l > 1.0):
@@ -143,52 +162,66 @@ def calc_p(k, i, filtering = True): # assume filtering (interest aggregation) is
 			# print("pre exp: " + str(exponent))
 
 			# FROM TECHNICAL REPORT
-			# exponent = exponent * (p_k_l * (1.0 - p_filter_k_l))
+			exponent = exponent * (p_k_l * (1.0 - p_filter_k_l))
 
-			# FROM PAPER
-			exponent = exponent * p_k_l 
+			# FROM PAPER #1
+			# exponent = exponent * p_k_l 
 
+			# # FROM PAPER #2
+			# exponent = exponent * (1.0 - p_filter_k_l)
 
 			# print("post exp: " + str(exponent))
 
 		# p = p_k(1) ^ exponent
-		# print("base, exp")
-		# print(pk1)
-		# print(exponent)
-		# print(pow(pk1, exponent))
-		# print("done")
-		# time.sleep(1)
 		return pow(pk1, exponent)
 	else:
-		raise Exception("Non-filtering model not implemented.")
+		pk1 = calc_p1(k) # p_k(1)^f \equiv p_k(1) (no previous filtering has occurred)
 
-def calc_rt(i):
-	global rts
-	return 2.0 * rts * i
+		# Sanity check
+		if (pk1 > 1.0):
+			raise Exception("Pk(1) can't be larger than 1.0")
+
+		# Calculate the exponent
+		exponent = float(1.0) 
+		for l in range(1, i): #[1, i - 1]
+			p_k_l = calc_p(k, l, filtering)
+
+			# More sanity checks
+			if (p_k_l > 1.0):
+				raise Exception("ERROR: miss probability invalid: " + str(p_k_l))
+
+			# Accumulate the exponent
+			exponent = exponent * p_k_l 
+
+		# p = p_k(1) ^ exponent
+		return pow(pk1, exponent)
+
+def calc_rt(i): # just assume a link rate of 1ms (global rts can have some pull on this)
+	return 2.0 * i
 
 # the model assumes that all links have the same round trip delay, which is fine
 # this is the virtual RTT for a single chunk, which is ultimately part of a piece of content somehow, depending on fragmentation
 
 # Status: Validated.
-def calc_VRTT(k):
+def calc_VRTT(k, filtering = True):
 	global N
-	global rts
 	s = 0.0
 
 	# Compute the weighted sum
+	print >> sys.stderr, "Calculating VRTT for k = " + str(k)
 	for i in range(1, N + 1): #[1, N]
-		pki = calc_p(k, i)
-		# print("k,i,pki = " + str(k) + ","+ str(i) + "," + str(pki))
-		if (pki > 1.0):
-			raise Exception("Probabilities (pki) can't be larger than one")
+		pp = calc_p(k, i, filtering)
+		if (pp > 1.0):
+			raise Exception("Probabilities (p(k, i)) can't be larger than one")
 		rti = calc_rt(i) # $R_i$
-		prod = float(1.0)
+		prod = 1.0
 		for j in range(1, i): #[1, i - 1]
-			pkj = calc_p(k, j)
+			pkj = calc_p(k, j, filtering)
 			if (pkj > 1.0):
 				raise Exception("Probabilities (pkj) can't be larger than one")
 			prod = prod * pkj
-		s = s + (rti * (1 - pki) * prod)
+		# s = s + (rti * (1.0 - pp) * prod)
+		s = s + (rti * (1.0 - pp))
 
 	# Safety check
 	if (s < 0):
@@ -197,53 +230,79 @@ def calc_VRTT(k):
 
 	return s
 
-def calcAverageThroughput(W, k):
-	return W / calc_VRTT(k)
+def calc_RVRTT(k, i, filtering):
+	global N
+	s = 0.0
 
-def calcTime(sigma, W, k):
-	X = calcAverageThroughput(W, k)
-	return sigma / (W * X)
+	for j in range(i, N + 1): # [i, N]
 
-def main():
-	# Read parameters in from a file and calculate accordingly
-	global N 
-	global M 
-	global rts 
-	global K 
-	global alpha 
-	global c 
-	global x 
-	global sigma 
-	global sigma_k 
-	global lmbda 
-	global lmbda_k 
-	global delta 
-	global delta_k 
+		if (j == 1):
+			filtering = True
+		else:
+			filtering = False
 
-	print("Populating with some test data...")
+		rii = (calc_rt(j) - calc_rt(i - 1))
+		pp = calc_p(k, j, filtering)
+		prod = 1.0
+		for l in range(i, j): # [i, j - 1]
+			prod = prod * calc_p(k, l, filtering)
+		s = s + (rii * (1.0 - pp) * prod)
 
-	# Taken from the paper... let's see if this works.
-	M = 20000
-	K = 400
-	alpha = float(1.2)
-	lmbda = float(40)
+	return s
+
+def calc_delta(k, i, filtering):
+	global delta
+	return min(delta, calc_RVRTT(k, i, filtering))
+
+def init_1():
+	global N, M, rts, K, alpha, c, x, sigma, sigma_k, lmbda, lmbda_k, delta 
+
+	M = 2000
+	K = 1000
+	alpha = 2.0
+	lmbda = 40.0
 	W = 1.0
-	x = float(200000) # 2GBs
-	sigma = float(690) # 10Kb chunks on average... but ideally, we'd like to model this using some random distribution
+	sigma = 10.0 * (10**3)   # 10kB chunk size for everything
+	x = 2000 * sigma
 	c = 1.0 
 	N = 3 # levels of the binary tree
-	rts = float(2) # 2ms round trip time between nodes
-	delta = float(1) 
+	rts = 2.0 # 2ms round trip time between nodes
+	delta = 1.0 
 
 	# Generate the data sets for each specific class of content items...
 	sigma_k = []
-	delta_k = [] # technically, delta_k = min(delta, VRTT_k), but we need delta_k to compute VRTT_k... cycle.
 	lmbda_k = []
 	for i in range(K):
 		sigma_k.append(sigma)
-		delta_k.append(delta)
-		# lmbda_k.append(lmbda)
-		lmbda_k.append(lmbda * calc_q(i + 1))
+		lmbda_k.append(lmbda * calc_q(i + 1)) # see the paper for details
+
+def init_2():
+	global N, M, rts, K, alpha, c, x, sigma, sigma_k, lmbda, lmbda_k, delta 
+
+	M = 20000
+	K = 10000
+	alpha = 2.0
+	lmbda = 40.0
+	W = 1.0
+	sigma = 10.0 * (10**3)   # 10kB chunk size for everything
+	x = 2000 * sigma
+	c = 1.0 
+	N = 3 # levels of the binary tree
+	rts = 2.0 # 2ms round trip time between nodes
+	delta = 1.0 
+
+	# Generate the data sets for each specific class of content items...
+	sigma_k = []
+	lmbda_k = []
+	for i in range(K):
+		sigma_k.append(sigma)
+		lmbda_k.append(lmbda * calc_q(i + 1)) # see the paper for details
+
+def main():
+	global N, M, rts, K, alpha, c, x, sigma, sigma_k, lmbda, lmbda_k, delta 
+
+	# Toggle which version to run
+	init_1()
 
 	# Do the calculations
 	n1 = []
@@ -257,25 +316,38 @@ def main():
 	vrtt3 = []
 	vrtt = []
 	for k in range(1, K + 1):
-		vrtt.append(calc_p(k, 2))
-		# mu1.append(calc_mu(k, 1, True))
-		# mu2.append(calc_mu(k, 2, True))
-		# n1.append(calc_p(k, 1))
-		# n2.append(calc_p(k, 2))
-		# n3.append(calc_p(k, 3))
+		vrtt.append(calc_VRTT(k))
+
+		# miss rates
+		mu1.append(calc_mu(k, 1, True) * sigma)
+		mu2.append(calc_mu(k, 2, True) * sigma)
+		mu3.append(calc_mu(k, 3, True) * sigma)
+		
+		# miss probabilities
+		n1.append(calc_p(k, 1))
+		n2.append(calc_p(k, 2))
+		n3.append(calc_p(k, 3))
 
 	# Show the plot
 	x = range(K)
 	fig = plt.figure()
 	ax1 = fig.add_subplot(111)
-	# ax1.scatter(x, n1, s=10, c='b', marker="s")
-	# ax1.scatter(x, vrtt, s=10, c='r', marker="s")
-	# ax1.scatter(x, mu1, s=10, c='g', marker="s")
-	ax1.scatter(x, vrtt, s=10, c='b', marker="s")
-	plt.show()
 
-	# for v in lmbda_k:
-	# 	print(v)
+	# VRTT
+	ax1.scatter(x, vrtt, s=10, c='r', marker="s")
+
+	# Miss rates
+	# ax1.scatter(x, mu1, s=10, c='g', marker="s")
+	# ax1.scatter(x, mu2, s=10, c='r', marker="s")
+	# ax1.scatter(x, mu3, s=10, c='b', marker="s")
+
+	# Miss probabilities
+	# ax1.scatter(x, n1, s=10, c='g', marker="s")
+	# ax1.scatter(x, n2, s=10, c='r', marker="s")
+	# ax1.scatter(x, n3, s=10, c='b', marker="s")
+
+	# Render the figure for display...
+	plt.show()
 
 if __name__ == "__main__":
 	main()
